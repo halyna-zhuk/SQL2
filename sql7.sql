9d0f0a618739977a9d3f97b7272fa465b53f8abf
Halyna Zhuk <104450263+halyna-zhuk@users.noreply.github.com>
2022-07-11 19:40:11 +0300

    Update sql7.sql

--- sql7.sql
+++ sql7.sql
@@ -1,9 +1,7 @@
-SELECT name 
-FROM people 
+SELECT name FROM people 
 WHERE id IN 
 (SELECT person_id 
 FROM stars 
 WHERE movie_id = 
-(SELECT id 
-FROM movies 
-WHERE title = 'Toy Story'));
+(SELECT id FROM movies 
+WHERE title = 'Batman'));
